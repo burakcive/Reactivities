4942d2b67ec0bf896c60ba2bfda996c138f2f9a1
Burak Cive <burakcive@gmail.com>
2020-02-20 00:15:11 +0300

    first commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dbContext;
+
+        public ValuesController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET: api/Values
+        [HttpGet]
+        public async Task<IEnumerable<Value>> Get()
+        {
+            var values = await _dbContext.Values.ToListAsync();
+            return values;
+        }
+
+        // GET: api/Values/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<Value> Get(int id)
+        {
+            var value = await _dbContext.Values.FindAsync(id);
+            return value;
+        }
+
+        // POST: api/Values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
